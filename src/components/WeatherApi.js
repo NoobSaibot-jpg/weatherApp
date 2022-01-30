d4e017fdb2d58500e66b182c2dec55164c0b17e2
NoobSaibot <kra4kas@gmail.com>
2022-01-30 23:30:08 +0200

    create proj

---
+++ src/components/WeatherApi.js
@@ -0,0 +1,54 @@
+import Spiner from './spiner/Spiner';
+import './weather.scss'
+import { useState, useEffect } from 'react';
+
+export default function WeatherApi() {
+
+
+  const [weather, setWeather]= useState({});
+
+  useEffect(()=>{
+    fetch("https://api.weatherapi.com/v1/current.json?key=b23e4af68e1f4e088d4132115222201&q=Kharkiv&aqi=no")
+    .then(res => res.json())
+    .then(
+      (result) => {
+        setWeather({
+          isLoaded: true,
+          items: result.current
+        });
+      },
+      (error) => {
+        setWeather({
+          isLoaded: true,
+          error
+        });
+      }
+    )
+  },[weather])
+  
+
+  const style = {
+    height: '100%',
+    width: '100%',
+    display: 'block'
+    
+  }
+
+    {if (weather.error) {
+      return <div>Ошибка: {weather.error.message}</div>
+    } else if (!weather.isLoaded) {
+      return <Spiner/>;
+    } else {
+      return (
+        <span style={style} className={weather.items.is_day? 'day_winter': 'night_winter'}>
+          {weather.items.condition.text}
+          <img src={weather.items.condition.icon} alt="img" />
+          {weather.items.temp_c}
+        </span>
+      )
+    }}
+
+
+  }
+
+  // condition":{"text":"Sunny","icon":"//cdn.weatherapi.com/weather/64x64/day/113.png"
---
+++ src/components/spiner/Spiner.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import './spiner.scss'
+
+export default function Spiner() {
+  return (
+    <div className="spinner-box">
+    <div className="solar-system">
+      <div className="earth-orbit orbit">
+        <div className="planet earth"></div>
+        <div className="venus-orbit orbit">
+          <div className="planet venus"></div>
+          <div className="mercury-orbit orbit">
+            <div className="planet mercury"></div>
+            <div className="sun"></div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+  );
+}
